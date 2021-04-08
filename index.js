20f7adaaf53b6cd4fae1b2e608ef6c4037d2bb5f
THEUZINMAKER2 <79604248+THEUZINMAKER2@users.noreply.github.com>
2021-04-08 18:52:45 -0200

    Add files via upload

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ const vcard = 'BEGIN:VCARD\n'
             + 'VERSION:3.0\n' 
             + 'FN:theuzinmaker\n' // Nama kamu
             + 'ORG:Lexa;\n' // Nama bot
-            + 'TEL;type=CELL;type=VOICE;waid=151366923806:+1 5136692380\n' //Nomor whatsapp kamu
+            + 'TEL;type=CELL;type=VOICE;waid=+151366923806:+1 5136692380\n' //Nomor whatsapp kamu
             + 'END:VCARD'
 prefix = '.','!','#'
 blocked = []
@@ -300,11 +300,11 @@ async function starts() {
 			}
 
 			const botNumber = client.user.jid
-			const ownerNumber = ["554792091566@s.whatsapp.net"] // replace this with your number
-			const mod = [ownerNumber,"554792091566@s.whatsapp.net"]//ubah nomor lo
-			const adminbotnumber = ["554792091566@s.whatsapp.net"]//ubah nomor lo
-			const frendsowner = ["554792091566@s.whatsapp.net"]//ubah nomor lo
-			const premium = ["554792091566@s.whatsapp.net","558296156440@s.whatsapp.net"] 
+			const ownerNumber = ["15136692380@s.whatsapp.net"] // replace this with your number
+			const mod = [ownerNumber,"15136692380@s.whatsapp.net"]//ubah nomor lo
+			const adminbotnumber = ["15136692380@s.whatsapp.net"]//ubah nomor lo
+			const frendsowner = ["15136692380@s.whatsapp.net"]//ubah nomor lo
+			const premium = ["15136692380@s.whatsapp.net","5519971528262@s.whatsapp.net"] 
 			const isGroup = from.endsWith('@g.us')
 			const sender = isGroup ? mek.participant : mek.key.remoteJid
 			const groupMetadata = isGroup ? await client.groupMetadata(from) : ''
