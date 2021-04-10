086eb3e1a1d2c8d5fa9da51a6514e277dd8c74e8
THEUZINMAKER2 <79604248+THEUZINMAKER2@users.noreply.github.com>
2021-04-10 01:12:24 -0200

    Add files via upload

--- src/iklan.js
+++ src/iklan.js
@@ -4,13 +4,13 @@ const iklan = () => {
 ║
 ╠═══════════════════════════
 ╠➥ *LISTA DE ALUGUEL E CRIAR BOTS:*
-╠➥ *ALUGUEL: 10 / GRUPO (MÊS)*
-╠➥ *CRIAR: 35 (PODE SER PROPRIETÁRIO)*
+╠➥ *ALUGUEL: 5 / GRUPO (MÊS)*
+╠➥ *CRIAR: 10 (PODE SER PROPRIETÁRIO)*
 ╠➥ *PODE PAGAR ATRAVÉS DE:*
 ╠➥ *MERCADO PAGO, BOLETO,*
 ╠═══════════════════════════
 ╠➥ *VANTAGENS*
-╠➥ *wa.me/554792091566*
+╠➥ *wa.me/15136692380
 ║
 ╚═〘  THZBOT  〙
 `
